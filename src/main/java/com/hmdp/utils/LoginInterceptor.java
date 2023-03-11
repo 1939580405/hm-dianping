5276ffad2dbdec0d6a42e1b469c90f70b3571569
XieHao <1939580405@qq.com>
2023-03-11 10:22:32 +0800

    SMS login function completed

--- src/main/java/com/hmdp/utils/LoginInterceptor.java
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -15,14 +15,17 @@ import java.util.concurrent.TimeUnit;
 public class LoginInterceptor implements HandlerInterceptor {
     private StringRedisTemplate stringRedisTemplate;
 
+    public LoginInterceptor() {
+    }
+
     public LoginInterceptor(StringRedisTemplate stringRedisTemplate) {
         this.stringRedisTemplate = stringRedisTemplate;
     }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        //1. 获取session
-        //TODO 1.获取请求头中的token
+       /* //1. 获取session
+        // 1.获取请求头中的token
         //HttpSession session = request.getSession();
         String token = request.getHeader("authorization");
         if(StrUtil.isBlank(token)){
@@ -30,17 +33,17 @@ public class LoginInterceptor implements HandlerInterceptor {
             return false;
         }
         //2.获取session中的用户
-        //TODO 2.基于TOKEN获取redis中的用户
+        // 2.基于TOKEN获取redis中的用户
         //Object user = session.getAttribute("user");
         String key = RedisConstants.LOGIN_USER_KEY+ token;
         Map<Object, Object> userMap = stringRedisTemplate.opsForHash().entries(key);
 
         //3. 判断用户是否存在
-        /*if (user == null){
+        *//*if (user == null){
             //4. 不存在，拦截
             response.setStatus(401);
             return false;
-        }*/
+        }*//*
         if(userMap.isEmpty()){
             response.setStatus(401);
             return false;
@@ -48,13 +51,21 @@ public class LoginInterceptor implements HandlerInterceptor {
 
 
         //5. 存在 保存用户信息到ThreadLocal,不同线程
-        //TODO 5.将查询到的Hash数据转为UserDTO对象
+        //5.将查询到的Hash数据转为UserDTO对象
         UserDTO userDTO = BeanUtil.fillBeanWithMap(userMap, new UserDTO(),false);
         UserHolder.saveUser(userDTO);
         //6. 放行
 
-        //TODO 7.刷新Token有效期
+        //7.刷新Token有效期
         stringRedisTemplate.expire(key, RedisConstants.LOGIN_USER_TTL, TimeUnit.MINUTES);
+        return true;*/
+        //1.判断是否需要拦截就行（ThreadLoacl中是否有用户）
+        if(UserHolder.getUser() == null){
+            //没有用户就拦截
+            response.setStatus(401);
+            return false;
+        }
+        //有就放行
         return true;
     }
 
